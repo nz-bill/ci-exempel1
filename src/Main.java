15721464072015fd5b298e704e4964b0f4a753d3
nz-bill <bill.palmestedt@zocom.se>
2025-04-28 11:51:17 +0200

    added new print

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // ny kommentarr
-        System.out.println("Hello world!");
+        System.out.println("Hello github");
         //mer kommentarer
     }
 }
